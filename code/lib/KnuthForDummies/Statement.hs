82ea9eabb3f1e710d275526ac8c0127ac1c65302
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-12-14 10:24:40 +1300

    Smart constructor of statements

--- code/lib/KnuthForDummies/Statement.hs
+++ code/lib/KnuthForDummies/Statement.hs
@@ -1,12 +1,21 @@
 {-
     Types and functions relating to statements.
 -}
+{-# LANGUAGE RecordWildCards #-}
 
 module KnuthForDummies.Statement where
 
--- Imports
-import Data.Vector.Unboxed as U
-
 -- A statement
-data Statement = Statement { numAtoms :: Int, bits :: U.Vector Bool }
+data Statement = Statement { numAtoms :: Int, bits :: [Bool] }
+
+-- Smart constructor for statements
+makeStatement :: Int                -- Number of atoms
+              -> [Int]              -- Which atoms are included
+              -> Maybe Statement
+makeStatement n included
+    | n <= 0 = Nothing
+    | any (<0) included || any (>= n) included = Nothing
+    | otherwise = Just Statement {..} where
+        numAtoms = n
+        bits     = [i `elem` included | i <- [0..n]]
 
